55bfca37064024d05b1d5d28b9b2924c0f2c3e2c
Pouapheng Yang <phewy06@yahoo.com>
2021-06-04 16:05:19 -0500

    addtocollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  const album = {album:title, artist, yearPublished}
+  collection.push(title, artist, yearPublished);
+  console.log(` ${title} was added to collection`);
+  return collection
+}
+
+addToCollection ('Meteora', 'Linkin Park', '2003')
+addToCollection ('How to Save a Life', 'The Fray','2005')
