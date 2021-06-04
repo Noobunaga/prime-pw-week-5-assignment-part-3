e55cd96be88a0ac579578bf8b6da6e2cd4fb15d4
Pouapheng Yang <phewy06@yahoo.com>
2021-06-04 16:24:49 -0500

    addtocollection2

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,4 +10,16 @@ function addToCollection(title, artist, yearPublished) {
 }
 
 addToCollection ('Meteora', 'Linkin Park', '2003')
+
+addToCollection ('Hybrid Theory','Linkin Park','2000')
 addToCollection ('How to Save a Life', 'The Fray','2005')
+addToCollection ('Kane Brown', 'Kane Brown', '2016')
+addToCollection ('Ticket to L.A.','Brett Young','2018')
+addToCollection ('Metallica','Metallica','1991')
+addToCollection ('Spider-Man:Into the Spider-verse OST','Various Artist','2018')
+
+console.log(collection);
+
+function showCollection(){
+  
+}
